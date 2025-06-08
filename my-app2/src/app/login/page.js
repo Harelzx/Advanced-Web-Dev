c3483f9076d21f4772beb008bc893855ac6bd064
harelzx <harelzx41@gmail.com>
2025-06-08 11:24:27 +0300

    refactor(login): Remove saving name to sessionStorage

--- my-app2/src/app/login/page.js
+++ my-app2/src/app/login/page.js
@@ -33,7 +33,6 @@ const Login = () => {
             const role = userData.role;
             sessionStorage.setItem('user', true);
             sessionStorage.setItem('uid', user.user.uid);
-            sessionStorage.setItem('Name', userData.name);
             setEmail('');
             setPassword('');
             if (role === 'teacher') {
